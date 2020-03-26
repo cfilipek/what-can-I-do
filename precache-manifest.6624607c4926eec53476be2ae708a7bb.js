47885d6c09664927697323eecb4950c70c89d5d0
Claire Filipek <cfilipek@sva.edu>
2020-03-26 17:22:37 -0400

    Updates

---
+++ precache-manifest.6624607c4926eec53476be2ae708a7bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41da38818676e8924f9a972a1804fbb",
+    "url": "/what-can-I-do/index.html"
+  },
+  {
+    "revision": "49eecec11f4d7f9f519a",
+    "url": "/what-can-I-do/static/css/main.5ef92726.chunk.css"
+  },
+  {
+    "revision": "54dd5cdac6980d11836e",
+    "url": "/what-can-I-do/static/js/2.f377ca4f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/what-can-I-do/static/js/2.f377ca4f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49eecec11f4d7f9f519a",
+    "url": "/what-can-I-do/static/js/main.4ab4a25f.chunk.js"
+  },
+  {
+    "revision": "7fa0166967c708d995cc",
+    "url": "/what-can-I-do/static/js/runtime-main.cfc3c39f.js"
+  }
+]);
